84f4704fd0dc3db3a34c624660ddb646b7473b11
LomaxRx <andrewlomaxr@gmail.com>
2018-03-18 19:37:53 -0400

    main app setup

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Route, Link, Redirect } from 'react-router-dom'
+import Subheader, { MenuList } from '../components/Subheader';
 
 const App = () => (
   <div className="app">
-    
+    <Subheader>
+      <MenuList color="dark-blue" items={[
+        { label: 'People', linkTo: '/people/' },
+        { label: 'Things', linkTo: '/things/' },
+        { label: 'Reports', linkTo: '/reports/' },
+        { label: 'Glossary', linkTo: '/glossary/' },
+        { label: 'Network', linkTo: '/network/' },
+      ]}/>
+    </Subheader>
   </div>
 )
 
---
+++ src/app/components/Subheader.js
@@ -0,0 +1,5 @@
+import { MenuList } from '../../components/Subheader';
+import Subheader from '../../components/Subheader';
+
+export { MenuList };
+export default Subheader;
