aabbb66411b96dbe4bd606361491663defa59405
LomaxRx <andrewlomaxr@gmail.com>
2018-07-07 11:08:07 -0400

    relationship tag animation

--- src/components/RelationshipTags.js
+++ src/components/RelationshipTags.js
@@ -3,31 +3,13 @@ import './RelationshipTags.css';
 import React from 'react';
 
 export class RelationshipTagGroup extends React.Component {
-  render(){
-    let { size='' } = this.props
-    return (
-      <div className={`relationship-tag-group ${size}`}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-export class RelationshipTag extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       expanded: false
     }
   }
-  shortenedTag = () => {
-    let label = this.props.label || this.props.type;
-    if(this.props.sm && !this.state.expanded){
-      if(label.length > 10) return `${label.substring(0,9)}...`;
-    }
 
-    return label;
-  }
   expand = () => {
     this.setState({ expanded: true });
   }
@@ -37,9 +19,32 @@ export class RelationshipTag extends React.Component {
   }
 
   render(){
+    let { size='' } = this.props
+    return (
+      <div onMouseEnter={this.expand} onMouseLeave={this.collapse}
+        className={`relationship-tag-group ${size}`}>
+        {this.props.children.map((c,i)=>{
+          return React.cloneElement(c, {expanded: this.state.expanded, sm: size==='small' });
+        })}
+      </div>
+    );
+  }
+}
+
+export class RelationshipTag extends React.Component {
+  shortenedTag = () => {
+    let label = this.props.label || this.props.type;
+    if(this.props.sm && !this.props.expanded){
+      if(label.length > 10) return `${label.substring(0,9)}...`;
+    }
+
+    return label;
+  }
+
+  render(){
     let { type, label, empty } = this.props
     return (
-      <div className={`relationship-tag ${type}${empty? ' empty': ''}`} onMouseEnter={this.expand} onMouseLeave={this.collapse}>
+      <div className={`relationship-tag ${type}${empty? ' empty': ''}`}>
         <div>{this.shortenedTag()}</div>
       </div>
     );
